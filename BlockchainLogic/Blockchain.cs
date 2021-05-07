196b6ce9a140dfb8371d797ca4338135bfcad03c
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-05-07 19:54:46 +0200

    Create blockchain class to operate transaction pool

---
+++ BlockchainLogic/Blockchain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace BlockchainLogic
+{
+    public class Blockchain
+    {
+        /// <summary>
+        /// Transaction pool
+        /// </summary>
+        public List<Transaction> TransactionPool { get; set; }
+
+        public Blockchain()
+        {
+            TransactionPool = new List<Transaction>();
+        }
+
+        /// <summary>
+        /// Add a transaction to pool
+        /// </summary>
+        /// <param name="trx">Transaction</param>
+        public void AddTransactionPool(Transaction trx)
+        {
+            TransactionPool.Add(trx);
+        }
+        
+    }
+}
