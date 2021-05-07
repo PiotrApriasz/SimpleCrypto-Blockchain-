e8db2628cce1411d56a04256fcf24fa1d26dc9fe
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-05-07 19:37:25 +0200

    Create Transaction class and add properties

---
+++ BlockchainLogic/Transaction.cs
@@ -0,0 +1,15 @@
+﻿namespace BlockchainLogic
+{
+    public class Transaction
+    {
+        #region Properties
+
+        public long TimeStamp { get; set; }
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+        public double Amount { get; set; }
+        public double Fee { get; set; }
+
+        #endregion
+    }
+}
