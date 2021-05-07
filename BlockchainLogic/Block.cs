f8fbe9e1bab62afbefaa00674d89ebd658c32d4a
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-05-07 21:39:28 +0200

    Create block class and add properties

---
+++ BlockchainLogic/Block.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BlockchainLogic
+{
+    public class Block
+    {
+        #region Properties
+
+        /// <summary>
+        /// Sequence number of blocks
+        /// </summary>
+        public int Height { get; set; }
+        /// <summary>
+        /// The time when the block was created
+        /// </summary>
+        public Int64 TimeStamp { get; set; }
+        /// <summary>
+        /// Hash of previous block
+        /// </summary>
+        public byte[] PrevHash { get; set; }
+        /// <summary>
+        /// Unique Hash of the block
+        /// </summary>
+        public byte[] Hash { get; set; }
+        /// <summary>
+        /// Collections of transactions that occur
+        /// </summary>
+        public Transaction[] Transactions { get; set; }
+        /// <summary>
+        /// Creator of block identified by the public key
+        /// </summary>
+        public string Creator { get; set; }
+
+        #endregion
+    }
+}
