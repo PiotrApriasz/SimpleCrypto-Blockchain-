0de9e04325e5e9e6b5ea0a61a12fc2c9b5cf2f0a
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-05-07 19:46:05 +0200

    Add console interface

---
+++ CLIInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CLIInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
