39a95be94e3e819f08c0d46a6419a9d090268fea
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-05-25 01:54:42 +0200

    Add LiteDb to the project
    Create Library to perform data base operations
    Creat class DbAccess

---
+++ DataBaseManager/DbAccess.cs
@@ -0,0 +1,13 @@
+﻿using LiteDB;
+
+namespace DataBaseManager
+{
+    public class DbAccess
+    {
+        public static LiteDatabase Db { get; set; }
+        public const string DbName = @"node.db";
+        public const string TblBlocks = "tbl_blocks";
+        public const string TblTransactionPool = "tbl_transaction_pool";
+        public const string TblTransactions = "tbl_transactions";
+    }
+}
